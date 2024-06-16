76af5b02ce7e26c0c26ace947a3820d116a20dff
Mostafa Negim <mostafa.negim@gmail.com>
2024-06-16 23:45:52 +0330

    Add: listener

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		go handler(conn)
+	}
+}
+
+func handler(conn net.Conn) {
+	fmt.Println("new connection from", conn.RemoteAddr())
 }
