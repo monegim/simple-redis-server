cf821ff135606e1a0a7bc3e1c38a65750d586d17
Mostafa Negim <mostafa.negim@gmail.com>
2024-06-17 19:25:58 +0330

    Add: RESP type

---
+++ resp.go
@@ -0,0 +1,57 @@
+package main
+
+type RESP struct {
+	Message string
+	Type    RespType
+}
+type RespType string
+
+const (
+	RESP_SIMPLE_STRING RespType = "RESP_SIMPLE_STRING"
+	RESP_ERROR         RespType = "RESP_ERROR"
+	RESP_INTEGER       RespType = "RESP_INTEGER"
+	RESP_BULK_STRING   RespType = "RESP_BULK_STRING"
+	RESP_ARRAYS        RespType = "RESP_ARRAYS"
+)
+
+func NewRESP(msg string) *RESP {
+	t := getType(msg)
+	return &RESP{
+		Message: msg[1:],
+		Type:    t,
+	}
+}
+
+func (r *RESP) ToString() string {
+	switch r.Type {
+	case RESP_SIMPLE_STRING:
+		return "+" + r.Message
+	case RESP_ERROR:
+		return "-" + r.Message
+	case RESP_INTEGER:
+		return ":" + r.Message
+	case RESP_BULK_STRING:
+		return "*" + r.Message
+	case RESP_ARRAYS:
+		return "*" + r.Message
+	default:
+		return ""
+	}
+}
+func getType(s string) RespType {
+	c := s[0]
+	switch c {
+	case '+':
+		return RESP_SIMPLE_STRING
+	case '-':
+		return RESP_ERROR
+	case ':':
+		return RESP_INTEGER
+	case '$':
+		return RESP_BULK_STRING
+	case '*':
+		return RESP_ARRAYS
+	default:
+		return ""
+	}
+}
