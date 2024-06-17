e7c544c6e24bdc596b2c4f787998c25947fc2e28
Mostafa Negim <mostafa.negim@gmail.com>
2024-06-17 13:43:04 +0330

    Add: client server

--- main.go
+++ main.go
@@ -1,26 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 )
 
-func main() {
-	listener, err := net.Listen("tcp", ":6379")
+type Server struct {
+	host string
+	port string
+}
+
+type Client struct {
+	conn net.Conn
+}
+
+type Config struct {
+	Host string
+	Port string
+}
+
+func New(config *Config) *Server {
+	return &Server{host: config.Host, port: config.Port}
+}
+
+func (s *Server) Run() {
+	listener, err := net.Listen("tcp", s.host+":"+s.port)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//defer listener.Close()
+	defer listener.Close()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
-		go handler(conn)
+		client := &Client{conn: conn}
+		go client.handleRequest()
 	}
 }
 
-func handler(conn net.Conn) {
-	fmt.Println("new connection from", conn.RemoteAddr())
+func (client *Client) handleRequest() {
+	reader := bufio.NewReader(client.conn)
+	for {
+		message, err := reader.ReadString(byte("\r\n"))
+		if err != nil {
+			client.conn.Close()
+			log.Println(err)
+			return
+		}
+		fmt.Printf("message incomming: %s", message)
+		client.conn.Write([]byte(fmt.Sprintf("message incomming: %s", message)))
+	}
+}
+
+func main() {
+	server := New(&Config{
+		Host: "127.0.0.1",
+		Port: "6379",
+	})
+	server.Run()
+
 }
