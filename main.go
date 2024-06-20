724c635d901792651414ac9c9ba8e5c858e7629e
Mostafa Negim <mostafa.negim@gmail.com>
2024-06-20 16:27:22 +0330

    Add: support for concurrent

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -20,6 +21,7 @@ type Client struct {
 
 type Store struct {
 	data map[string]Fields
+	lock *sync.RWMutex
 }
 type Fields struct {
 	Value  any
@@ -44,7 +46,7 @@ func ToClient(s string, t RespType) []byte {
 	}
 }
 func (c *Commands) Ping() []byte {
-	return []byte("+PONG\r\n")
+	return ToClient("ping", RESP_SIMPLE_STRING)
 }
 func New(config *Config) *Server {
 	return &Server{host: config.Host, port: config.Port}
@@ -78,7 +80,6 @@ func (client *Client) handleRequest() {
 	reader := bufio.NewReader(client.conn)
 	for {
 		message, err := reader.ReadString('\n')
-		log.Println("message: ", message)
 		if err != nil {
 			client.conn.Close()
 			log.Println(err)
@@ -92,14 +93,13 @@ func (client *Client) handleRequest() {
 				log.Fatal(err)
 			}
 		} else if counter <= 2*numberOfElements && counter%2 == 0 {
+			log.Println("message:", message)
 			commands.AddCommand(strings.TrimSuffix(message, "\r\n"))
 			//inputCommand = append(inputCommand, )
 		}
 		counter++
 		if commands.Length() == numberOfElements {
 			returnString = commands.ExecCommand()
-			log.Println("input command:", commands)
-			log.Println("output:", string(returnString))
 			client.conn.Write(returnString)
 		}
 	}
@@ -130,6 +130,8 @@ func (c *Commands) Set() error {
 	//TODO: Support for EX, NX, XX
 	key := (*c)[1]
 	value := (*c)[2]
+	store.lock.Lock()
+	defer store.lock.Unlock()
 	store.data[key] = Fields{
 		Value: value,
 	}
@@ -138,6 +140,8 @@ func (c *Commands) Set() error {
 
 func (c *Commands) GET() ([]byte, error) {
 	key := (*c)[1]
+	store.lock.RLock()
+	defer store.lock.RUnlock()
 	value, ok := store.data[key]
 	if !ok {
 		return c.Nil(), nil
