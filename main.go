8790dbb1e5a882de96b1ffc6a27943f0eda03e74
Mostafa Negim <mostafa.negim@gmail.com>
2024-06-21 22:56:51 +0330

    Update: refactor code

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ type Server struct {
 }
 
 type Client struct {
-	conn net.Conn
+	conn   net.Conn
+	reader *bufio.Reader
 }
 
 type Store struct {
@@ -32,8 +33,8 @@ type Config struct {
 	Port string
 }
 
-type Commands []string
-type ServerResponse []byte
+// type Commands []string
+//type ServerResponse []byte
 
 func ToClient(s string, t RespType) []byte {
 	switch t {
@@ -45,9 +46,10 @@ func ToClient(s string, t RespType) []byte {
 		return []byte("$-1\r\n")
 	}
 }
-func (c *Commands) Ping() []byte {
-	return ToClient("ping", RESP_SIMPLE_STRING)
-}
+
+//	func (c *Commands) Ping() []byte {
+//		return ToClient("ping", RESP_SIMPLE_STRING)
+//	}
 func New(config *Config) *Server {
 	return &Server{host: config.Host, port: config.Port}
 }
@@ -64,116 +66,150 @@ func (s *Server) Run() {
 			log.Fatal(err)
 		}
 		client := &Client{conn: conn}
-		go client.handleRequest()
+		go client.serve()
 	}
 }
 
 var (
-	err                       error
-	store                     = Store{data: map[string]Fields{}}
-	counter, numberOfElements int
-	returnString              []byte
-	commands                  Commands
+	err          error
+	store        = Store{data: map[string]Fields{}}
+	returnString []byte
+	//commands     Commands
 )
 
-func (client *Client) handleRequest() {
-	reader := bufio.NewReader(client.conn)
+func (client *Client) serve() {
+	client.reader = bufio.NewReader(client.conn)
 	for {
-		message, err := reader.ReadString('\n')
+		cmd, err := client.readCommand()
 		if err != nil {
-			client.conn.Close()
-			log.Println(err)
-			return
+			log.Fatal(err)
 		}
-		if strings.HasPrefix(message, "*") {
-			counter = 0
-			numberOfElements, err = strconv.Atoi(strings.TrimSpace(message[1:]))
-			commands.Clear()
-			if err != nil {
-				log.Fatal(err)
+		log.Println("cmd:", cmd)
+		client.conn.Write([]byte("$-1\r\n"))
+	}
+}
+
+//func (c *Commands) Clear() {
+//	*c = nil
+//}
+//func (c *Commands) AddCommand(command string) {
+//	*c = append(*c, command)
+//}
+//func (c *Commands) Length() int {
+//	return len(*c)
+//}
+//
+//func (c *Commands) Nil() []byte {
+//	return []byte("$-1\r\n")
+//}
+//
+//func (c *Commands) Echo() []byte {
+//	return []byte("+" + strings.Join((*c)[1:], " ") + "\r\n")
+//}
+//
+//func (c *Commands) OK() []byte {
+//	return []byte("+OK\r\n")
+//}
+//
+//func (c *Commands) Set() error {
+//	//TODO: Support for EX, NX, XX
+//	key := (*c)[1]
+//	value := (*c)[2]
+//	store.lock.Lock()
+//	defer store.lock.Unlock()
+//	store.data[key] = Fields{
+//		Value: value,
+//	}
+//	return nil
+//}
+//
+//func (c *Commands) GET() ([]byte, error) {
+//	key := (*c)[1]
+//	store.lock.RLock()
+//	defer store.lock.RUnlock()
+//	value, ok := store.data[key]
+//	if !ok {
+//		return c.Nil(), nil
+//	}
+//	return ToClient(value.Value.(string), RESP_SIMPLE_STRING), nil
+//}
+//func (c *Commands) ExecCommand() []byte {
+//	if c.Length() == 0 {
+//		return c.Nil()
+//	}
+//	if c.Length() == 1 && (*c)[0] == "ping" {
+//		return c.Ping()
+//	}
+//	switch (*c)[0] {
+//	case "echo":
+//		return c.Echo()
+//	case "set":
+//
+//		err = c.Set()
+//		if err != nil {
+//			return []byte("-Notset\r\n")
+//		}
+//		log.Printf("sore: %#v", store)
+//		return c.OK()
+//	case "get":
+//		v, err := c.GET()
+//		if err != nil {
+//			return ToClient(err.Error(), RESP_ERROR)
+//		}
+//		return v
+//	default:
+//		return c.Nil()
+//	}
+//}
+
+type Command struct {
+	Name string
+	Args []string
+}
+
+func (client *Client) readCommand() (*Command, error) {
+	var numberOfElements int
+	for {
+		line, err := client.reader.ReadString('\n')
+		log.Println(line)
+		if err != nil {
+			return nil, err
+		}
+
+		if strings.HasPrefix(line, "*") {
+			numberOfElements, err = strconv.Atoi(strings.TrimSpace(line[1:]))
+			//commands.Clear()
+			log.Println(numberOfElements)
+			command := &Command{}
+			for i := 0; i < 2*numberOfElements; i++ {
+				line, err := client.reader.ReadString('\n')
+				if err != nil {
+					return nil, err
+				}
+				if i == 1 {
+					command.Name = strings.TrimSuffix(line, "\r\n")
+				}
+				if i%2 == 1 && i > 2 {
+					command.Args = append(command.Args, strings.TrimSpace(line))
+				}
 			}
-		} else if counter <= 2*numberOfElements && counter%2 == 0 {
-			log.Println("message:", message)
-			commands.AddCommand(strings.TrimSuffix(message, "\r\n"))
-			//inputCommand = append(inputCommand, )
-		}
-		counter++
-		if commands.Length() == numberOfElements {
-			returnString = commands.ExecCommand()
-			client.conn.Write(returnString)
+
+			return command, nil
 		}
 	}
 }
-func (c *Commands) Clear() {
-	*c = nil
-}
-func (c *Commands) AddCommand(command string) {
-	*c = append(*c, command)
-}
-func (c *Commands) Length() int {
-	return len(*c)
-}
-
-func (c *Commands) Nil() []byte {
-	return []byte("$-1\r\n")
-}
-
-func (c *Commands) Echo() []byte {
-	return []byte("+" + strings.Join((*c)[1:], " ") + "\r\n")
-}
-
-func (c *Commands) OK() []byte {
-	return []byte("+OK\r\n")
-}
-
-func (c *Commands) Set() error {
-	//TODO: Support for EX, NX, XX
-	key := (*c)[1]
-	value := (*c)[2]
-	store.lock.Lock()
-	defer store.lock.Unlock()
-	store.data[key] = Fields{
-		Value: value,
-	}
-	return nil
-}
-
-func (c *Commands) GET() ([]byte, error) {
-	key := (*c)[1]
-	store.lock.RLock()
-	defer store.lock.RUnlock()
-	value, ok := store.data[key]
-	if !ok {
-		return c.Nil(), nil
-	}
-	return ToClient(value.Value.(string), RESP_SIMPLE_STRING), nil
-}
-func (c *Commands) ExecCommand() []byte {
-	if c.Length() == 0 {
-		return c.Nil()
-	}
-	if c.Length() == 1 && (*c)[0] == "ping" {
-		return c.Ping()
-	}
-	switch (*c)[0] {
-	case "echo":
-		return c.Echo()
-	case "set":
-
-		err = c.Set()
+func (client *Client) readLine() (string, error) {
+	var line string
+	for {
+		partialLine, isPrefix, err := client.reader.ReadLine()
 		if err != nil {
-			return []byte("-Notset\r\n")
+			return "", nil
 		}
-		log.Printf("sore: %#v", store)
-		return c.OK()
-	case "get":
-		v, err := c.GET()
-		if err != nil {
-			return ToClient(err.Error(), RESP_ERROR)
+		line += string(partialLine)
+		if isPrefix {
+			continue
 		}
-		return v
-	default:
-		return c.Nil()
+		return line, nil
 	}
 }
 
